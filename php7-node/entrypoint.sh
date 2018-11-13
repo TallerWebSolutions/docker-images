e9e4d201611dfe6d5e97bea829b891706ed86633
Luiz Henrique Guerra <lhguerra@gmail.com>
2018-11-13 16:54:36 -0200

    Added apache restart command to entrypoint script.

--- php7-node/entrypoint.sh
+++ php7-node/entrypoint.sh
@@ -5,6 +5,11 @@ set -e
 # Source NVM scripts
 source /taller/.nvm/nvm.sh
 
+# Start services and loggers.
+# ---------------------------
+
+sudo service apache2 restart > /tmp/apache2.log
+
 echo ""
 echo "--------------------------------------"
 echo "--- Virtual Machine ready to work! ---"
